77bacb94fa46cce26ef76e4c83882ef29f024867
Erik Whiting <erik@erikwhiting.com>
2022-04-24 20:10:21 -0500

    Handle line breaks in GenBank writer

--- Tests/test_GenBank.py
+++ Tests/test_GenBank.py
@@ -26,6 +26,8 @@ from Bio import SeqIO
 from Bio.GenBank import Record
 from Bio.Seq import Seq
 from Bio.Seq import UndefinedSequenceError
+from Bio.SeqFeature import FeatureLocation
+from Bio.SeqFeature import SeqFeature
 from Bio.SeqRecord import SeqRecord
 
 
@@ -7990,6 +7992,24 @@ KEYWORDS    """,
         with open("GenBank/DS830848.gb") as fh:
             orig_first_line = fh.readline().strip()
         self.assertEqual(first_line, orig_first_line)
+
+    def test_line_break_in_features(self):
+        """Handling line breaks."""
+        record = SeqRecord(
+            Seq("AAAA"),
+            annotations={"molecule_type": "DNA"},
+            features=[
+                SeqFeature(
+                    FeatureLocation(1, 2),
+                    type="misc_feature",
+                    qualifiers={"label": "line\nbreak"},
+                )
+            ],
+        )
+        out_handle = StringIO()
+        SeqIO.write([record], out_handle, "genbank")
+        generated_label = out_handle.getvalue().split("/label=")[1].split("\n")[0]
+        self.assertEqual(generated_label, '"line break"')
 
     def test_qualifier_order(self):
         """Check the qualifier order is preserved."""
